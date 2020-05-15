3f056bb7aaf6751055f576a16f883d5ecbf3415f
NaveenChander <naveenchander@gmail.com>
2020-05-14 22:37:00 -0700

    Implemented interface to allow unit testing of core and added proc to getServiceAuth

---
+++ db/procs/usp_GetServiceOFferingsByIDOrApikey.sql
@@ -0,0 +1,59 @@
+USE [GoKube]
+GO
+
+/****** Object:  StoredProcedure [Customer].[usp_GetServiceOFferingsByIDOrApikey]    Script Date: 4/23/2020 1:38:04 PM ******/
+IF EXISTS(SELECT *
+FROM sys.procedures
+WHERE name = 'usp_GetServiceOFferingsByIDOrApikey')
+BEGIN
+    DROP PROCEDURE [Customer].[usp_GetServiceOFferingsByIDOrApikey]
+END
+GO
+
+/****** Object:  StoredProcedure [Customer].[usp_GetServiceOFferingsByIDOrApikey]    Script Date: 4/23/2020 1:38:04 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+/****************************************************************************
+
+DATE        VERSION       AUTHOR                REFERENCE
+
+2020-05-14  1.0           Naveen Prabhaker
+
+TEST CODE 
+-- EXEC [Customer].[usp_GetServiceOFferingsByIDOrApikey] @apiKey = 'ZJBdn&6ckT&!2S(K4Qc^vJSyn'
+
+Initial Version
+
+*****************************************************************************/
+ 
+
+CREATE PROCEDURE [Customer].[usp_GetServiceOFferingsByIDOrApikey] (
+@apiKey VARCHAR(500) = '',
+@CustomerID INT = 0
+)
+AS
+BEGIN
+
+	SELECT 
+		Distinct
+		[ServiceOfferingID] = SO.ServiceOfferingID, 
+		[Name] = SO.Name, 
+		[Description] = ISNULL(SO.ServiceDescription, SO.Name), 
+		[InternalReferenceID] = ISNULL(SO.InternalServiceRecordID,0)
+	FROM 
+		Customer.CustomerCredentials CC
+	INNER JOIN Customer.CustomerServiceOfferings CSO
+		ON CSO.CustomerID = CC.CustomerID
+	INNER JOIN ServiceOfferings.ServiceOfferings SO
+		ON SO.ServiceOfferingID = CSO.CustomerServiceOfferingID
+	WHERE 	
+		(@apiKey = '' OR CC.ClientAPIKey = @apiKey)
+		OR
+		(@CustomerID = 0 OR  CC.CustomerID = @CustomerID)
+
+END
