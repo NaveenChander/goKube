73116995effed425ec429a0daa3db2557be062fa
NaveenChander <naveenchander@gmail.com>
2020-06-15 03:08:36 -0700

    Implemented Redis

---
+++ core/redisCache.go
@@ -0,0 +1,68 @@
+package core
+
+import (
+	"log"
+	"os"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+var pool *redis.Pool
+
+func initPool() {
+	pool = &redis.Pool{
+		MaxIdle:   80,
+		MaxActive: 12000,
+		Dial: func() (redis.Conn, error) {
+			conn, err := redis.Dial("tcp", "localhost:6379")
+			if err != nil {
+				log.Printf("ERROR: fail init redis pool: %s", err.Error())
+				os.Exit(1)
+			}
+			return conn, err
+		},
+	}
+}
+
+func setCache(key string, val string, expirationInSeconds int) error {
+
+	if pool == nil {
+		initPool()
+	}
+
+	// get conn and put back when exit from method
+	conn := pool.Get()
+	defer conn.Close()
+
+	_, err := conn.Do("SET", key, val)
+	if err != nil {
+		log.Printf("ERROR: fail set key %s, val %s, error %s", key, val, err.Error())
+		return err
+	}
+
+	_, err = conn.Do("EXPIRE", key, expirationInSeconds)
+	if err != nil {
+		log.Printf("ERROR: fail set key %s, val %s, error %s", key, val, err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func getCache(key string) (string, error) {
+	if pool == nil {
+		initPool()
+	}
+
+	// get conn and put back when exit from method
+	conn := pool.Get()
+	defer conn.Close()
+
+	s, err := redis.String(conn.Do("GET", key))
+	if err != nil {
+		log.Printf("ERROR: fail get key %s, error %s", key, err.Error())
+		return "", err
+	}
+
+	return s, nil
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/my/repo
+
+go 1.14
+
+require (
+	github.com/davidnarayan/go-flake v0.0.0-20180604195229-c680a61bf75c // indirect
+	github.com/denisenkom/go-mssqldb v0.0.0-20200428022330-06a60b6afbbc // indirect
+	github.com/go-redis/redis v6.15.8+incompatible // indirect
+	github.com/go-redis/redis/v8 v8.0.0-beta.5
+	github.com/golang/protobuf v1.3.3
+	github.com/gomodule/redigo v1.8.2 // indirect
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/naveenchander/GoKube v0.0.0-20200608214657-591a4c3f90ed
+)
